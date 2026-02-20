0cf655e01add011f5c3127272e3d473f93b09038
TueTran2008 <tuetd174343@gmail.com>
2026-02-21 01:45:41 +0700

    Update new NVChad version

--- nvim/lua/custom/init.lua
+++
@@ -1,9 +0,0 @@
-vim.g.dap_virtual_text = true
-vim.g.autoformat = true
--- vim.opt.colorcolumn = "200"
-
--- Disable persistent undo for files in /private directory
-vim.api.nvim_create_autocmd("BufReadPre", { pattern = "/private/*", command = "set noundofile" })
-
--- Enable persistent undo for other files
-vim.opt.undofile = true
